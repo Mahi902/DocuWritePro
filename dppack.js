02e4fc1b5bdb414dc38c62c41260cc14777d3463
Mohtadi Azad Mahi <mohtadiazadmahi902@gmail.com>
2026-01-30 14:38:54 +0600

    Update dppack.js

--- dppack.js
+++ dppack.js
@@ -1,6 +1,6 @@
 <script>
 // ===== DP ICON PACK =====
-// Version: 1.1
+// Version: 1.2
 // All icons and designs reserved by mahi902
 const DP_ICONS = {
   menu: `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><rect x="3" y="5" width="18" height="2" rx="1"/><rect x="3" y="11" width="18" height="2" rx="1"/><rect x="3" y="17" width="18" height="2" rx="1"/></svg>`,
@@ -222,6 +222,35 @@ const DP_ICONS = {
 
   // --- Tech & Infrastructure ---
   google: `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09zM12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23zM5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84zM12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/></svg>`,
+
+  // --- Archives & Data ---
+  'file-zip': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-6 6h-2v2h2v-2zm0-2h-2v2h2v-2zm0-2h-2v2h2V8zm2 8h-4v2h4v-2z"/></svg>`,
+  'file-csv': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-3 14H9v-1h2v1zm3 0h-2v-1h2v1zm0-2h-5v-1h5v1zm2-4H7V7h9v3z"/></svg>`,
+  'file-json': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 15h-2v-1h2v1zm2-3h-4v-1h4v1zm0-3h-4V8h4v1zM13 3.5L18.5 9H13V3.5z"/></svg>`,
+  'file-xml': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm1 14l-1.5-1.5L13 16l-1.5-1.5L10 16l-2-2 2-2 1.5 1.5L13 12l1.5 1.5L16 12l2 2-3 2z"/></svg>`,
+
+  // --- Web & Styles ---
+  'file-css': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm-1 12h-2v1h2v1h-3v-4h3v1h-2v1h2v1zm5-1.5h-3v1h2v1h-3v-4h3v1h-2v1h2v1z"/></svg>`,
+  'file-html': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-2 15h-1v-4h-1v4H9v-5h1v2h1v-2h1v5zm4 0h-3v-1h1v-3h-1v-1h3v1h-1v3h1v1z"/></svg>`,
+  'file-js': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm.1 16.5c-.7 0-1.2-.2-1.6-.6l.7-.7c.3.3.6.4.9.4.4 0 .6-.2.6-.5v-4h1v3.9c0 1-.7 1.5-1.6 1.5zm3.4-.1c-.6 0-1.1-.2-1.5-.6l.6-.7c.3.3.6.4.9.4.3 0 .5-.1.5-.4s-.1-.4-.5-.5l-.4-.1c-.7-.2-1.1-.5-1.1-1.2 0-.7.6-1.2 1.4-1.2.5 0 .9.1 1.3.4l-.6.8c-.2-.2-.5-.3-.7-.3-.3 0-.5.1-.5.3s.1.3.5.5l.4.1c.7.2 1.1.6 1.1 1.2s-.6 1.3-1.4 1.3z"/></svg>`,
+  'file-ts': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm-1 14h-1v-4h-1v-1h3v1h-1v4zm4.5-.1c-.6 0-1.1-.2-1.5-.6l.6-.7c.3.3.6.4.9.4.3 0 .5-.1.5-.4s-.1-.4-.5-.5l-.4-.1c-.7-.2-1.1-.5-1.1-1.2 0-.7.6-1.2 1.4-1.2.5 0 .9.1 1.3.4l-.6.8c-.2-.2-.5-.3-.7-.3-.3 0-.5.1-.5.3s.1.3.5.5l.4.1c.7.2 1.1.6 1.1 1.2s-.6 1.3-1.4 1.3z"/></svg>`,
+
+  // --- Programming Languages ---
+  'file-py': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-3 15H9v-5h2v2h1v-2h1v5h-2v-2h-1v2zm4-4h-2v-1h2v1z"/></svg>`,
+  'file-php': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-5 13H8v-4h1v1.5h1V11h1v4h-1v-1.5H9V15z"/></svg>`,
+  'file-java': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-4 13l-1-4h1l.5 2 1-2h1l-1.5 4h-1z"/></svg>`,
+  'file-c': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 14h-2c-1.1 0-2-.9-2-2v-1c0-1.1.9-2 2-2h2v1h-2v3h2v1z"/></svg>`,
+  'file-cpp': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 14h-2v-1h2v1zm4 0h-1v1h-1v-1h-1v-1h1v-1h1v1h1v1z"/></svg>`,
+
+  // --- Design Assets ---
+  'file-svg': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 12.5h-2V13h2v-1h-3v4h3v-1.5zM17 16l-2-4h-1l2 4h1z"/></svg>`,
+  'file-figma': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-2 15a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0-3.5a1.5 1.5 0 1 1 1.5-1.5c0 .8-.7 1.5-1.5 1.5z"/></svg>`,
+  'file-ai': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-4 13H9l-1-4h1l.5 2.5 1-2.5h1l-1.5 4zm4 0h-1v-4h1v4z"/></svg>`,
+
+  // --- Media Formats ---
+  'file-mp3': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-2 15c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3-4h-3v-2h3v2z"/></svg>`,
+  'file-mp4': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm1 14h-1v-1h1v1zm-4 0H9v-5h3v1h-2v1h2v1h-2v2z"/></svg>`,
+  'file-gif': `<svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm-1 14h-2v-4h3v1h-2v2h2v1z"/></svg>`,
   };
 };
 
